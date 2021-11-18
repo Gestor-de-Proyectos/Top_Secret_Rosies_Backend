c04cd4e795f72455464180d1391c7783c63b9555
Yadira Henao <yadira.henao@udea.edu.co>
2021-11-18 15:31:51 -0500

    se agregan los demás modelos

--- index.ts
+++ index.ts
@@ -1,23 +1,34 @@
 import conectarBD from "./db/db";
-import { Enum_Rol } from "./models/enums";
+import { Enum_EstadoUsuario, Enum_TipoObjetivo, Enum_Rol } from "./models/enums";
 import { UserModel } from "./models/user";
+import { ProjectModel } from "./models/project";
 
 const main = async () => {
     await conectarBD();
 
-    await UserModel.create({
-        correo:"bethsyfalcon@gmail.com",
-        identificacion:"5678",
-        nombre:"Bethsy",
-        apellido:"Falcon",
-        rol: Enum_Rol.lider
-    })
-    .then((u) => {
-        console.log('usuario creado', u);        
-    })
-    .catch ((e) => {
-        console.error('Error creando el usuario', e);
-    });
+    const usuarioInicial = await UserModel.create({
+        nombre: 'Yadira',
+        apellido: 'Henao',
+        correo: 'yadira.h96@gmail.com',
+        identificacion: '10371111',
+        rol: Enum_Rol.administrador,
+        estado: Enum_EstadoUsuario.Autorizado,
+      });
+
+      const proyectoCreado = await ProjectModel.create({
+        nombre: 'Proyecto Mision TIC',
+        fechaInicio: new Date('2021/12/18'),
+        fechaFin: new Date('2022/12/18'),
+        presupuesto: 120000,
+        lider: usuarioInicial._id,
+        objetivos: [
+          { descripcion: 'Este es el objetivo general', tipo: Enum_TipoObjetivo.general },
+          { descripcion: 'Este es el objetivo especifico 1', tipo: Enum_TipoObjetivo.especifico },
+          { descripcion: 'Este es el objetivo especifico 2', tipo: Enum_TipoObjetivo.especifico },
+        ],
+      });
+
+      console.log('proyecto creado', proyectoCreado);
 };
 
 main();
--- models/enums.ts
+++ models/enums.ts
@@ -9,8 +9,34 @@ enum Enum_Rol {
     Autorizado = 'Autorizado',
     No_Autorizado = 'No_Autorizado',
   }
+
+  enum Enum_EstadoProyecto {
+    activo = 'Activo',
+    inactivo = 'Inactivo',
+  }
+
+  enum Enum_FaseProyecto {
+    iniciado = 'Iniciado',
+    desarrollo = 'En Desarrollo',
+    terminado = 'Terminado',
+    nula = '',
+  }
+
+  enum Enum_TipoObjetivo {
+    general = 'General',
+    especifico = 'Especifico',
+  }
+
+  enum Enum_EstadoInscripcion {
+    aceptada = 'Aceptada',
+    rechazada = 'Rechazada',
+  }
   
   export {
     Enum_Rol,
     Enum_EstadoUsuario,
+    Enum_EstadoProyecto,
+    Enum_EstadoInscripcion,
+    Enum_FaseProyecto,
+    Enum_TipoObjetivo
   };
