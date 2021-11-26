8a31174791e9c5f041787f8551aa02d5a1479d31
Bethsyf <bethsyfalcon@gmail.com>
2021-11-26 18:37:43 -0500

    crud objetivos2

--- models/usuario/resolvers.js
+++ models/usuario/resolvers.js
@@ -2,8 +2,19 @@ import { UserModel } from './usuario.js';
 
 const resolversUsuario = {
   Query: {
-    Usuarios: async (parent, args) => {
-      const usuarios = await UserModel.find();
+    Usuarios: async (parent, args, context) => {
+      const usuarios = await UserModel.find().populate([
+        {
+          path: 'inscripciones',
+          populate: {
+            path: 'proyecto',
+            populate: [{ path: 'lider' }, { path: 'avances' }],
+          },
+        },
+        {
+          path: 'proyectosLiderados',
+        },
+      ]);
       return usuarios;
     },
     Usuario: async (parent, args) => {
